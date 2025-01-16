3abefc9f41255ba4230fbba7d1a99b5b0c2bb31f
Berthold Mboneng <berthnk@gmail.com>
2025-01-16 09:54:14 +0100

    Changed CV Document

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import { Toaster } from "react-hot-toast"
 
 export const metadata: Metadata = {
   title: "Berthold's Corner",
@@ -17,6 +18,7 @@ export default function RootLayout({
         suppressHydrationWarning={true} // This prevents extensions from causing a server/client mismatch
       >
         {children}
+        <Toaster />
       </body>
     </html>
   )
